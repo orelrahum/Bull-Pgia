a6b71714dacef95827496c5941839b6da2c6e661
orelrahum <orel.rahum@gmail.com>
2019-04-30 11:07:55 +0300

    add test

--- Test.cpp
+++ Test.cpp
@@ -32,8 +32,42 @@ int main() {
 		ConstantGuesser g1234{"1234"}, g12345{"12345"}, g9999{"9999"};
 
 		testcase.setname("Calculate bull and pgia")
+		.CHECK_OUTPUT(calculateBullAndPgia("123456","123456"), "6,0")      // 6 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("123456","123645"), "3,3")      // 3 bull, 3 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("123456","456123"), "0,6")      // 0 bull, 6 pgia
+
+		.CHECK_OUTPUT(calculateBullAndPgia("12345","12345"), "5,0")      // 5 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("12345","43218"), "0,4")      // 0 bull, 4 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("50345","53406"), "1,3")      // 1 bull, 3 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("42356","40189"), "1,0")      // 1 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("47568","54769"), "1,3")      // 1 bull, 3 pgia
+
 		.CHECK_OUTPUT(calculateBullAndPgia("1234","1234"), "4,0")      // 4 bull, 0 pgia
 		.CHECK_OUTPUT(calculateBullAndPgia("1234","4321"), "0,4")      // 0 bull, 4 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("5034","5340"), "1,3")      // 1 bull, 3 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("4235","4018"), "1,0")      // 1 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("4756","5476"), "1,3")      // 1 bull, 3 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("1023","1032"), "2,2")      // 2 bull, 2 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("1234","1235"), "3,0")      // 3 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("1234","5734"), "2,0")      // 2 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("1234","5789"), "0,0")      // 0 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("1234","5671"), "0,1")      // 0 bull, 1 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("1234","5621"), "0,2")      // 0 bull, 2 pgia
+
+		.CHECK_OUTPUT(calculateBullAndPgia("123","123"), "3,0")      // 3 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("123","432"), "0,2")      // 0 bull, 2 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("503","534"), "1,1")      // 1 bull, 1 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("423","401"), "1,0")      // 1 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("475","547"), "0,3")      // 0 bull, 3 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("102","103"), "2,0")      // 2 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("123","573"), "1,0")      // 1 bull, 0 pgia
+
+		.CHECK_THROWS(calculateBullAndPgia("acsa","scsa"))
+		.CHECK_THROWS(calculateBullAndPgia("123a","1234"))
+		.CHECK_THROWS(calculateBullAndPgia("12345","12"))
+		.CHECK_THROWS(calculateBullAndPgia("abcd","ab34"))
+
+
 		;
 
 		testcase.setname("Play with dummy choosers and guessers")
@@ -48,6 +82,12 @@ int main() {
 		SmartGuesser smarty;
 		for (uint i=0; i<100; ++i) {
 			testcase.CHECK_EQUAL(play(randy, smarty, 4, 100)<=100, true);  // smarty should always win in at most 10 turns!
+		}
+				for (uint i=0; i<100; ++i) {
+			testcase.CHECK_EQUAL(play(randy, smarty, 2, 100)<=100, true);  // smarty should always win in at most 10 turns!
+		}
+				for (uint i=0; i<100; ++i) {
+			testcase.CHECK_EQUAL(play(randy, smarty, 3, 100)<=100, true);  // smarty should always win in at most 10 turns!
 		}
 
     grade = testcase.grade();
