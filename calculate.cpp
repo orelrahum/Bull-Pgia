b2bd58fa05a754f5aab3faa8e5e8dbbe752b2e8b
NaomiO <44748207+NaomiO@users.noreply.github.com>
2019-05-05 09:08:28 +0300

    Update calculate.cpp
    my commit

--- calculate.cpp
+++ calculate.cpp
@@ -1,13 +1,46 @@
-#include "calculate.hpp"
-#include <iostream>
 
-namespace bullpgia 
-{
+#include <cstdio>
+#include <string>
+#include <iostream>
+#include <math.h>
+#include "calculate.hpp"
+
+namespace bullpgia{
 
   string calculateBullAndPgia(string choice, string guess){
-	
-        string s = "";
-        return s;
- }
 
-};
+	//if any of the strings are empty, the game cannot be played
+	if( choice == "" || guess == "" ) throw std::invalid_argument( "The string is empty" );
+		
+	int bull = 0;
+	int pgiot = 0;
+		
+	//for the string selected by chooser
+	for( int i = 0; i < choice.length(); i++ ){
+		int runnerInt = choice[i];
+			
+		
+		for( int j = 0; j < guess.length(); j++ ){
+			int guessInt = guess[j];
+				
+				
+			if( runnerInt == guessInt){
+				if( i == j ){ //position match
+					bulls++;
+				}
+				else{ //contains relation
+					pgiot++;
+				}
+			}
+		}
+	}
+		
+	string b = to_string(bulls);
+        string c = to_string(pgiot);
+        string s = b + "," + c;
+        return s;
+
+	}
+
+
+}
