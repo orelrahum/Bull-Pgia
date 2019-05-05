46a1a977212c29a3f63f0def967c7804c87f4522
NaomiO <44748207+NaomiO@users.noreply.github.com>
2019-05-05 09:40:07 +0300

    Update calculate.hpp
    change

--- calculate.hpp
+++ calculate.hpp
@@ -8,9 +8,9 @@ namespace bullpgia
 {
 
  string calculateBullAndPgia(string choice, string guess);
- int getNumExactCharMatches(string chosen, string guess);
- int getNumSimilarChars(string chosen, string guess);
- int* getCharCounts(string str);
+// int getNumExactCharMatches(string chosen, string guess);
+// int getNumSimilarChars(string chosen, string guess);
+// int* getCharCounts(string str);
 
 }
 
