1fc920f95bb7df9dfbf90405a7686bb79168ecca
NaomiO <44748207+NaomiO@users.noreply.github.com>
2019-05-05 09:12:48 +0300

    Update calculate.hpp
    committ

--- calculate.hpp
+++ calculate.hpp
@@ -2,12 +2,17 @@
 
 #include<iostream>
 using std::string;
-
+//using namespace std;
 
 namespace bullpgia 
 {
 
  string calculateBullAndPgia(string choice, string guess);
+ int getNumExactCharMatches(string chosen, string guess);
+ int getNumSimilarChars(string chosen, string guess);
+ int* getCharCounts(string str);
+
+}
 
 };
 
